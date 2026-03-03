dc06d1f8f1ff02ecfdf3bea4f1615f1311d3a08b
MariemZaouali <zaouali.meriam@gmail.com>
2026-03-03 11:33:09 +0100

    Change image file extension from .jpg to .tif
    Edit the extention from jpg to tif

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -164,9 +164,9 @@ class ChangeDataset(data.Dataset):
         return img_A_ts, img_B_ts, label_ts
 
     def load_img_and_mask(self, file_name):
-        img_A = Image.open(os.path.join(self.img_A_path_list, file_name + '.jpg'))
-        img_B = Image.open(os.path.join(self.img_B_path_list, file_name + '.jpg'))
-        label = Image.open(os.path.join(self.gt_path_list, file_name + '.png'))
+        img_A = Image.open(os.path.join(self.img_A_path_list, file_name + '.tif'))
+        img_B = Image.open(os.path.join(self.img_B_path_list, file_name + '.tif'))
+        label = Image.open(os.path.join(self.gt_path_list, file_name + '.tif'))
 
         img_A, img_B, label = cv_random_flip(img_A, img_B, label)
         img_A, img_B, label = randomCrop(np.array(img_A), np.array(img_B), np.array(label))
