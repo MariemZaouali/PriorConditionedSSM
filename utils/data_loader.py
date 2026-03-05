Fix dynamic size broadcasting in mosaic augmentation

utils/data_loader.py
@@ -176,9 +176,9 @@ class ChangeDataset(data.Dataset):
         return img_A_ts, img_B_ts, label_ts
 
     def load_img_and_mask(self, file_name):
-        img_A = Image.open(os.path.join(self.img_A_path_list, file_name + '.png'))
-        img_B = Image.open(os.path.join(self.img_B_path_list, file_name + '.png'))
-        label = Image.open(os.path.join(self.gt_path_list, file_name + '.png'))
+        img_A = Image.open(os.path.join(self.img_A_path_list, file_name + '.tif'))
+        img_B = Image.open(os.path.join(self.img_B_path_list, file_name + '.tif'))
+        label = Image.open(os.path.join(self.gt_path_list, file_name + '.tif'))
 
         img_A, img_B, label = cv_random_flip(img_A, img_B, label)
         img_A, img_B, label = randomCrop(np.array(img_A), np.array(img_B), np.array(label))
@@ -208,9 +208,9 @@ class ChangeDataset(data.Dataset):
 
         for index, f_name in enumerate(file_name_list):
 
-            img_A = Image.open(os.path.join(self.img_A_path_list, f_name + '.png'))
-            img_B = Image.open(os.path.join(self.img_B_path_list, f_name + '.png'))
-            label = Image.open(os.path.join(self.gt_path_list, f_name + '.png'))
+            img_A = Image.open(os.path.join(self.img_A_path_list, f_name + '.tif'))
+            img_B = Image.open(os.path.join(self.img_B_path_list, f_name + '.tif'))
+            label = Image.open(os.path.join(self.gt_path_list, f_name + '.tif'))
 
             img_A, img_B, label = cv_random_flip(img_A, img_B, label)
 
@@ -281,7 +281,7 @@ class ChangeDataset(data.Dataset):
             # Always resize patch to match target region size (regardless of whether they match)
             # This ensures consistent dimensions for assignment
             if patch_h > 0 and patch_w > 0:
-                # Resize patch to match target region size
+                # Resize patch to match target region size exactly
                 patch_A = np.array(Image.fromarray(patch_A).resize((target_w, target_h), Image.BILINEAR))
                 patch_B = np.array(Image.fromarray(patch_B).resize((target_w, target_h), Image.BILINEAR))
                 patch_label = np.array(Image.fromarray(patch_label).resize((target_w, target_h), Image.NEAREST))